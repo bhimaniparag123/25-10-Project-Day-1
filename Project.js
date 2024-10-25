236f0cb5ee5814e16fc3a1208d4e82964c124351
bhimaniparag123 <159759757+bhimaniparag123@users.noreply.github.com>
2024-10-25 15:45:16 +0530

    Add files via upload
    25-10 Project Day-1

---
+++ Project.js
@@ -0,0 +1,85 @@
+//#1
+// const numbers = [3, 7, 10, 15, 21, 28, 33];
+// const greaterThanFifteen = numbers.filter(num => num > 15);
+// console.log(greaterThanFifteen);
+
+//output=======>Array(3)
+
+
+//#2
+// const nums = [2, 4, 6, 8];
+// const multipliedByThree = nums.map(num => num * 3);
+// console.log(multipliedByThree);
+
+//output=======>(4) [6,12,18,24]
+
+
+//#3
+// const fruits = ["apple", "banana", "grape", "mango"];
+// fruits.push("orange");
+// console.log(fruits);
+
+///output=======>(5) ['apple', 'banana', 'grape', 'mango', 'orange']
+
+
+//#4
+// const message = "    A javascript language is Awesome!    ";
+// const trimmedMessage = message.trim();
+// const lengthOfTrimmedMessage = trimmedMessage.length;
+
+// console.log("Trimmed Message:", trimmedMessage);
+// console.log("Length of Trimmed Message:", lengthOfTrimmedMessage);
+
+//output===========>Trimmed Message: A javascript language is Awesome!
+//                  Length of Trimmed Message: 33
+
+
+//#5
+// const str = "hello, world!";
+// const upperCaseStr = str.toUpperCase();
+// console.log(upperCaseStr);
+
+//output===========>HELLO, WORLD!
+
+
+//#6
+// const text = "I am learning JavaScript now!";
+// const extractedWord = text.slice(17, 27); // Start at index 17 and end at index 27
+// console.log(extractedWord);
+
+//output===========>aScript no
+
+
+//#7
+// let roundedUp = Math.ceil(5.3);
+// console.log("Rounded up (5.3):", roundedUp);
+// let roundedDown = Math.floor(7.9);
+// console.log("Rounded down (7.9):", roundedDown); 
+
+//output================>6,7
+
+
+//#8
+// function getRandomNumber() {
+//     return Math.floor(Math.random() * 51);
+//   }
+//   let randomNumber = getRandomNumber();
+//   console.log("Random number between 0 and 50:", randomNumber);
+
+//output================>Rendom number
+
+
+//#9
+// let currentDate = new Date();
+// let currentYear = currentDate.getFullYear();
+// console.log("Current Year:", currentYear);
+
+//output================>2024
+
+
+//#10
+// let date = new Date("2024-01-01");
+// date.setFullYear(2026);
+// console.log("Updated Date:", date);
+
+//output=================>Updated Date: Thu Jan 01 2026 05:30:00 GMT+0530 (India Standard Time)
